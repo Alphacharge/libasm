bc44352168918a5beb83b2f25cd0b6fe362241a3
Alphacharge <git.mytechnikinfo.de>
2025-03-24 12:52:55 +0100

    feat: new function strlen, we use asm because .s is convetion to at&tgit add src/

---
+++ src/ft_strlen.asm
@@ -0,0 +1,19 @@
+section .text
+global ft_strlen
+ft_strlen:
+	; set return value to 0
+	mov	rax, 0
+.loop:
+	;cpy value of rdi to cl register
+	mov	cl, [rdi]
+	; cmp to zero, value would be in ZF
+	cmp	cl, 0
+	je	.done
+	; inc counter
+	add	rax, 1
+	; move rdi pointer
+	add	rdi, 1
+	jmp	.loop
+.done:
+	; return rax
+	ret
