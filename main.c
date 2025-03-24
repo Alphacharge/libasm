5c45cd1fb3f596ebe83d34d2408864f43c039e69
Alphacharge <git.mytechnikinfo.de>
2025-03-24 13:53:07 +0100

    rename vars, add check for strcpy

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
 extern size_t	ft_strlen(const char *str);
+extern char		*ft_strcpy(char *dest, const char *src);
 
 int main() {
-	const char	*text = "Hello, world!";
+	const char	*src = "Hello, world!";
 	size_t		len;
 
-	len = ft_strlen(text);
-	printf("Length of string: %zu\n", len);
+	len = ft_strlen(src);
+	printf("Length of src is: %zu\n\n", len);
+
+	char		dest[len];
+	char		*check = NULL;
+
+	printf("src is:>%s<, dest is: >%s<, check is: >%s<\ncopy it...\n", src, dest, check);
+	check = ft_strcpy(dest, src);
+	printf("src is:>%s<, dest is: >%s<, check is: >%s<\n", src, dest, check);
+
 	return (0);
 }
